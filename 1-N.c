7ffdb1c84888fb22036bc5d31583ef1ba6d1cbb6
yashj1308 <45262601+yashj1308@users.noreply.github.com>
2022-06-02 22:36:49 +0530

    Create 1-N.c

---
+++ 1-N.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main() {
+    int x,i = 1;
+    printf("Enter the No. :\n");
+    scanf("%d",&x);
+    while (i <= x){
+        printf("%d",i);
+        printf("\n");
+        i++;
+    }
+}
